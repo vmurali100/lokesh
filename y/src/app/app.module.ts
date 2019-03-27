724d2daf532cdd92666750f10bdee85989f24a40
lokesh <garapati.lokeshkumar@gmail.com>
2019-03-27 18:22:33 +0530

    angular

---
+++ y/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp3Component } from './comp3/comp3.component';
+import { ParentComponent } from './comp3/parent/parent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Comp1Component,
+    Comp2Component,
+    Comp3Component,
+    ParentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
