9343e1606d855088f9515cdb7c092638641af89d
lokesh <garapati.lokeshkumar@gmail.com>
2019-03-30 15:58:31 +0530

    component commuication pract

---
+++ y/src/app/second-child/second-child.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-second-child',
+  templateUrl: './second-child.component.html',
+  styleUrls: ['./second-child.component.css']
+})
+export class SecondChildComponent implements OnInit {
+
+  @Input() recievedinfo;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+
+}
---
+++ y/src/app/second-parent/second-parent.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SecondParentComponent } from './second-parent.component';
+
+describe('SecondParentComponent', () => {
+  let component: SecondParentComponent;
+  let fixture: ComponentFixture<SecondParentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SecondParentComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SecondParentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
